1b8f01d2ce1774b611021054d5ee8578c4dbf05f
Rafael <rafaellessacastro@hotmail.com>
2023-09-08 18:19:34 -0300

    Add Pauta Entity with synchronize property as true

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { HelloController } from './hello.controller';
 import { HelloService } from './hello.service';
 import { PeopleModule } from './people/people.module';
 import { DatabaseModule } from './database/database.module';
+import { PautasModule } from './pautas/pautas.module';
 
 @Module({
-  imports: [PeopleModule, DatabaseModule],
+  imports: [PeopleModule, DatabaseModule, PautasModule],
   controllers: [AppController, HelloController],
   providers: [AppService, HelloService],
 })
---
+++ src/pautas/pauta.entity.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Pauta {
+
+    @PrimaryGeneratedColumn()
+    id?: string;
+
+    @Column()
+    description: string;
+    
+    @CreateDateColumn()
+    registration_date: Date;
+    
+    @Column({ type: 'timestamp', nullable: true })
+    opening_date?: Date;
+    
+    @Column({ type: 'timestamp', nullable: true })
+    closing_date?: Date;
+}
