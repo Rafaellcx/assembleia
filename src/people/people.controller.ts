934419ae1e7ca4e0c8568ab90e164dd4081bda35
Rafael <rafaellessacastro@hotmail.com>
2023-09-07 21:50:06 -0300

    Add person list

--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Res } from '@nestjs/common';
+import { PeopleService } from './people.service';
+import { Response } from 'express';
 @Controller('people')
-export class PeopleController {}
+export class PeopleController {
+    
+    constructor(private service: PeopleService)
+    {
+        this.service = service
+    }
+
+    @Get()
+    list(@Res() response: Response) {
+        const list = this.service.list();
+        return response.status(200).send(list);
+    }
+}
--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { Person } from './person';
 @Injectable()
-export class PeopleService {}
+export class PeopleService {
+    people: Person[] = [{id:1, name: "Fulano"}]
+
+    list(): Person[] {
+        return this.people;
+    }
+}
