88c6f35ca2c3aff955cfe2238b082e7d61e2d015
Rafael <rafaellessacastro@hotmail.com>
2023-09-07 22:27:10 -0300

    Add Find by id and save people functions

--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, Post, Body, Param } from '@nestjs/common';
 import { PeopleService } from './people.service';
-import { Response } from 'express';
+import { Response, response } from 'express';
+import { Person } from './person';
 @Controller('people')
 export class PeopleController {
     
-    constructor(private service: PeopleService)
-    {
+    constructor(private service: PeopleService) {
         this.service = service
     }
 
@@ -14,4 +14,20 @@ export class PeopleController {
         const list = this.service.list();
         return response.status(200).send(list);
     }
+
+    @Get('/:id')
+    getById(@Param('id') id: number, @Res() response: Response) {
+        const person = this.service.findById(id);
+
+        if(!person) return response.status(404).send()
+
+        return response.status(200).send(person);
+    }
+
+    @Post()
+    save(@Body() person: Person, @Res() response: Response) {
+        this.service.save(person);
+        return response.status(201).send("Save with success.")
+    }
+
 }
--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -2,9 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { Person } from './person';
 @Injectable()
 export class PeopleService {
-    people: Person[] = [{id:1, name: "Fulano"}]
-
+    people: Person[] = []
+    
     list(): Person[] {
         return this.people;
+    }
+    
+    findById(id: number): Person {
+        const foundPerson = this.people.find(function (person) {
+            return person.id == id
+        })
+        
+        return foundPerson
+    }
+    
+    save(person: Person) {
+        this.people.push(person);
     }
 }
