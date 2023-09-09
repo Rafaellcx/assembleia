e014d413f2f74b3737431c0c4f1d49795b275dda
Rafael <rafaellessacastro@hotmail.com>
2023-09-09 14:28:07 -0300

    Create Pauta Provider

---
+++ src/pautas/pauta.providers.ts
@@ -0,0 +1,15 @@
+import { DataSource, Repository } from "typeorm";
+import { Pauta } from "./pauta.entity"; 
+import { Provider } from "@nestjs/common";
+
+const pautaRepository: Provider<Repository<Pauta>> = {
+    provide: 'PAUTA_REPOSITORY',
+    useFactory: (dataSource: DataSource) => {
+        // const repository: Repository<Pauta> = dataSource.getRepository(Pauta);
+        // return repository;
+        return dataSource.getRepository(Pauta);
+    },
+    inject: ['DATA_SOURCE_MYSQL']
+}
+
+export const pautaProviders:Provider[] = [pautaRepository]
--- src/pautas/pautas.module.ts
+++ src/pautas/pautas.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
+import { pautaProviders } from './pauta.providers';
 
 @Module({
-    imports: [DatabaseModule]
+    imports: [DatabaseModule],
+    providers: []
 })
 export class PautasModule {}
