bc92dea0aa81aef1fcac020ed12f41347cf80d22
Rafael <rafaellessacastro@hotmail.com>
2023-09-09 14:42:17 -0300

    Create Pauta Service

--- src/pautas/pautas.module.ts
+++ src/pautas/pautas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { pautaProviders } from './pauta.providers';
+import { PautasService } from './pautas.service';
 
 @Module({
     imports: [DatabaseModule],
-    providers: []
+    providers: [PautasService, ...pautaProviders]
 })
 export class PautasModule {}
