0a16e7df4c698acb0915bc46641f783ba0ffff18
Rafael <rafaellessacastro@hotmail.com>
2023-09-12 06:17:27 -0300

    Create find and new associate functions

--- src/voto/associate/associate.service.ts
+++ src/voto/associate/associate.service.ts
@@ -7,5 +7,27 @@ export class AssociateService {
     constructor(
         @Inject('ASSOCIATE_REPOSITORY')
         private readonly associateRepository: Repository<Associate>
-    ){}
+    ) { }
+    
+    async obtainByCpf(cpf: string): Promise<Associate> {
+        return await this.associateRepository.findOne({
+            where: {
+                cpf: cpf
+            }
+        })
+    }
+
+    async recoveryOrCreate(cpf: string) : Promise<Associate> {
+        const associate: Associate = await this.obtainByCpf(cpf);
+
+        if (associate) {
+            return associate;
+        }
+
+        const newAssociate = new Associate();
+        newAssociate.cpf = cpf;
+        await this.associateRepository.save(newAssociate);
+
+        return newAssociate;
+    }
 }
