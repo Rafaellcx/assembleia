03f7354c65a7c51d8e8547986df94aec2198c8cd
Rafael <rafaellessacastro@hotmail.com>
2023-09-15 17:45:15 -0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {logger: ['log','error']});
   app.useGlobalPipes(new ValidationPipe());
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Assembleia API')
+    .setDescription('Study of an assembly microservice using NodeJs, NestJs, Swagger and Docker')
+    .setVersion('1.0.0')
+    .setContact('Rafael Lessa', '', 'rafaellessacastro@hotmail.com')
+    .build();
+  
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, swaggerDoc);
   await app.listen(3000);
 }
 bootstrap();
